7dc1841224833632659f904ff8f458934b2c7883
Anegasaki-n <144014837+Anegasaki-n@users.noreply.github.com>
2023-11-02 20:32:37 +0800

    Update 开发2.py

--- 开发2.py
+++ 开发2.py
@@ -8,6 +8,7 @@ driver.get('https://www.163.com/')
 x = driver.get_screenshot_as_base64()
 image = base64.b64decode(x)
 
+
 while True:
 	file = open('{}.jpg'.format(time.time()),'wb')
 	file.write(image)
