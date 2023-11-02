484a17c6afa959573264327d794f3aecab10dec3
Anegasaki-n <144014837+Anegasaki-n@users.noreply.github.com>
2023-11-02 20:15:26 +0800

    Create 开发2.py

---
+++ 开发2.py
@@ -0,0 +1,14 @@
+from selennium import webdriver
+import time
+import base64
+
+driver = webdriver.Chrome()
+driver.get('https://www.163.com/')
+
+x = driver.get_screenshot_as_base64()
+image = base64.b64decode(x)
+
+while True:
+	file = open('{}.jpg'.format(time.time()),'wb')
+	file.write(image)
+	time.sleep(300)
