9771a0de9d29454e8fd31c05a861339a1a71f7b7
Anegasaki-n <144014837+Anegasaki-n@users.noreply.github.com>
2023-11-02 14:52:28 +0800

    算法1.py

--- 算法1.py
+++ 算法1.py
@@ -1,21 +1,22 @@
 nums = list(map(int,input().strip('[]').split(',')))
 target = int(input)
 
+if sorted(nums) == nums:
 #   适用于无序列表
-for i in range(len(nums)):
-  for j in range(i+1,len(nums)):
-        if nums[i] + nums[j] == target:
-                return [i,j]
+	for i in range(len(nums)):
+  		for j in range(i+1,len(nums)):
+        		if nums[i] + nums[j] == target:
+                		return [i,j]
 
-'''  适用于有序列表
-for i in range(len(nums)):
-	if nums[i] > target/2:
-		temp1.append(nums[i])
-	else:
-		temp2.append(nums[i])
 
-for i in range(len(temp1)):
-	for j in range(len(temp2)):
-		if temp1[i] + temp2[j] == target:
-			print([j,i+j+1])
-'''
+else:
+	for i in range(len(nums)):
+		if nums[i] > target/2:
+			temp1.append(nums[i])
+		else:
+			temp2.append(nums[i])
+
+	for i in range(len(temp1)):
+		for j in range(len(temp2)):
+			if temp1[i] + temp2[j] == target:
+				print([j,i+j+1])
